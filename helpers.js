bd8f2faa95cbec9380cd33a31271ab82bd952bfa
Zea <78378462+zeamaria@users.noreply.github.com>
2021-04-22 23:40:06 -0700

    Tidy up code & refactor helpers

--- helpers.js
+++ helpers.js
@@ -1,6 +1,42 @@
+// HELPER FUNCTIONS 
+
+// Generates random string for User IDs and short URLS
+function generateRandomString(length) {
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+
+// Check if user is logged in by cookie
+// Note: this allows us to not have to constantly clear cookie cache on browser
+function getLoggedInUser(req, users) {
+  let user = {};
+  if(req.session.user && req.session.user !== "undefined" && typeof req.session.user !== "undefined") {
+    if(req.session.user.hasOwnProperty('id')) {
+      if(users[req.session.user.id]) {
+        user = req.session.user;
+      } else {
+        req.session = null;
+      }
+    }
+  }
+  return user;
+};
+
+// Returns the URLs where the userID is equal to the id of the currently logged-in user.
+function urlsForUser(id, urlDatabase){
+  let urlDatabaseForUser = {};
+  for (const key in urlDatabase) {
+    if(urlDatabase[key].userID === id) {
+      urlDatabaseForUser[urlDatabase[key].shortURL] = urlDatabase[key];
+    }
+  }
+  return urlDatabaseForUser;
+};
+
 // Checks if email is already in database
 function getUserByEmail(email, database) {
-  // loop through users and check if email exists
   for (const key in database) {
     if(database[key].email === email) {
       return database[key];
@@ -9,4 +45,6 @@ function getUserByEmail(email, database) {
   return false;
 }
 
-module.exports = { getUserByEmail };
+
+
+module.exports = { getUserByEmail, urlsForUser, getLoggedInUser, generateRandomString };
